a2ee49bf2e1c5daa808c8dc7fa69c5261c3c49bb
Mészáros Balázs <58610433+fireball90@users.noreply.github.com>
2023-09-14 10:02:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function aboutme() {
+    document.body.style.backgroundImage = "url('images/bg_1.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.getElementById('content2').style.backgroundImage = "url('images/bg_1_body.jpg')";
+  }
+
+function home() {
+    document.body.style.backgroundImage = "url('images/background.jpeg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.getElementById('content2').style.backgroundImage = "url('images/background_body.png')";
+}
+
+function gallery() {
+    document.body.style.backgroundImage = "url('images/bg_2.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.getElementById('content2').style.backgroundImage = "url('images/bg_2_body.jpg')";
+}
+
+function contact() {
+    document.body.style.backgroundImage = "url('images/bg_3.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.getElementById('content2').style.backgroundImage = "url('images/bg_3_body.jpg')";
+}
+
+function prices() {
+    document.body.style.backgroundImage = "url('images/bg_4.jpg')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.getElementById('content2').style.backgroundImage = "url('images/bg_4_body.jpg')";
+}
